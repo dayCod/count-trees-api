016e8efc4e84a869c78a6cc6d5d0a59bf2f3bdf1
dayCod <78329755+dayCod@users.noreply.github.com>
2023-07-04 12:00:07 +0700

    Create GlobalFunctions.php

---
+++ src/Helpers/GlobalFunctions.php
@@ -0,0 +1,11 @@
+<?php
+
+function http_request($url){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    curl_close($ch);
+
+    return $output;
+}
